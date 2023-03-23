93ec2e13bd5f6b8b2abd61029de054a8ad713eda
ocmino <ocmino@gmail.com>
2023-03-23 10:12:28 +0100

    sdfsdf

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import { ARButton, XR } from "@react-three/xr";
 import "./App.css";
 import HoleMask from "./HoleMask";
 
-
 function App() {
   return (
     <div className="App">
-    <ARButton sessionInit={{ optionalFeatures: ["dom-overlay", "local-floor", "hit-test"] }} />
+      <ARButton
+        sessionInit={{
+          optionalFeatures: ["dom-overlay", "local-floor", "hit-test"],
+        }}
+      />
       <Canvas>
-      <XR sessionInit={{ optionalFeatures: ["dom-overlay", "local-floor", "hit-test"] }}>
         <Environment preset="sunset" background={true} />
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <OrbitControls />
-        <HoleMask />
-      </XR>
+        <XR
+          sessionInit={{
+            optionalFeatures: ["dom-overlay", "local-floor", "hit-test"],
+          }}
+        >
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <OrbitControls />
+          <HoleMask />
+        </XR>
       </Canvas>
     </div>
   );
